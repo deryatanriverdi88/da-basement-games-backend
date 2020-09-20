577b865e31913dae276da58cbe649744554efb64
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-09-19 23:32:41 -0400

    creates index function for MagictheGatheringCards

--- app/controllers/magic_the_gatherig_cards_controller.rb
+++ app/controllers/magic_the_gatherig_cards_controller.rb
@@ -1,2 +1,6 @@
 class MagicTheGatherigCardsController < ApplicationController
+    def index
+        magic_the_gatherig_cards = MagicTheGatherigCardsController
+        render json:magic_the_gatherig_cards
+    end
 end
