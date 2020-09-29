3be3a012e3ab9ddc1ff61c60f39f8c68341a3719
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-09-29 01:31:31 -0400

    adjusts last_ten function

--- app/controllers/magic_the_gatherig_cards_controller.rb
+++ app/controllers/magic_the_gatherig_cards_controller.rb
@@ -5,6 +5,7 @@ class MagicTheGatherigCardsController < ApplicationController
     end
 
     def last_ten
-        render json:cards = MagicTheGatherigCard.all.first(10)
+        # render json:cards = MagicTheGatherigCard.default_order.all.slice(50000, MagicTheGatherigCard.default_order.all.length-1)
+        render json:cards = MagicTheGatherigCard.default_order.all.first(100)
     end
 end
