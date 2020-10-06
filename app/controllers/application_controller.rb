7e679ba0a4a4ca10f8b818680f49afa9277aafed
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 00:59:37 -0400

    adds encoded_token function for AppliccationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
     def user_payload(user)
         { user_id: user.id }
     end
+
+    def encoded_token(payload)
+        JWT.encode(payload, secret_key, "HS256")
+    end
 end
