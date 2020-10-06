d35f883aec19326d320a6b2bd995c3d3b1abaf6b
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 01:19:06 -0400

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_050230) do
+ActiveRecord::Schema.define(version: 2020_10_06_031056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_cards", force: :cascade do |t|
+    t.string "name"
+    t.string "img_url"
+    t.integer "category_id"
+    t.integer "product_id"
+    t.integer "group_id"
+    t.string "rarity"
+    t.string "sub_type"
+    t.string "color"
+    t.string "text"
+    t.boolean "foil"
+    t.string "group_name"
+    t.decimal "normal_low_price"
+    t.decimal "normal_mid_price"
+    t.decimal "normal_high_price"
+    t.decimal "normal_market_price"
+    t.decimal "foil_low_price"
+    t.decimal "foil_mid_price"
+    t.decimal "foil_high_price"
+    t.decimal "foil_market_price"
+    t.integer "amount"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "magic_the_gatherig_cards", force: :cascade do |t|
     t.string "name"
@@ -37,6 +63,13 @@ ActiveRecord::Schema.define(version: 2020_09_29_050230) do
     t.decimal "foil_mid_price"
     t.decimal "foil_high_price"
     t.decimal "foil_market_price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
