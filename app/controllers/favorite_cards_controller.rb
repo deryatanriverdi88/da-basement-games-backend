4cbc48a21f48992dd142f4781b27f2f33d985b08
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 01:13:25 -0400

    adds favorite_card_params function for favorite_cards_controller

--- app/controllers/favorite_cards_controller.rb
+++ app/controllers/favorite_cards_controller.rb
@@ -24,4 +24,10 @@ class FavoriteCardsController < ApplicationController
         favorite_card = FavoriteCard.find(params[:id])
         favorite_card.destroy
     end
+
+    private
+
+    def favorite_card_params
+        params.permit(:user_id, :magic_the_gatherig_id, :name, :img_url, :category_id, :product_id, :group_id, :rarity, :sub_type, :color, :text, :foil, :group_name, :normal_low_price, :normal_mid_price, :normal_high_price, :normal_market_price, :foil_low_price, :foil_mid_price, :foil_high_price, :foil_market_price, :amount)
+    end
 end
