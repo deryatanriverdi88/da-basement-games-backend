31fbff02d72bb5520c02e9cb82a2b620f4c85b75
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-06 01:09:40 -0400

    adds login function for SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def login
+        user = User.find_by(username: params[:username])
+        if user
+            password = params[:password]
+            is_authenticated = user.authenticate(password)
+            if is_authenticated
+                render json: { token: encoded_token(user_payload(user)), current_user: user}
+            else
+                render json: {error: "INCORRECT INFIORMATION, PLEASE TRY AGAIN"}
+            end
+        else
+            render json: {error: "INCORRECT INFIORMATION, PLEASE TRY AGAIN"}
+        end
+    end
 end
