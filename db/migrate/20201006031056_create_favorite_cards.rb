cb309a0e566be99c612fe8f6accf29032b206930
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 13:30:16 -0400

    updates the attributes of favorite cards table

--- db/migrate/20201006031056_create_favorite_cards.rb
+++ db/migrate/20201006031056_create_favorite_cards.rb
@@ -1,25 +1,7 @@
 class CreateFavoriteCards < ActiveRecord::Migration[6.0]
   def change
     create_table :favorite_cards do |t|
-      t.string "name"
-      t.string "img_url"
-      t.integer "category_id"
-      t.integer "product_id"
-      t.integer "group_id"
-      t.string "rarity"
-      t.string "sub_type"
-      t.string "color"
-      t.string "text"
-      t.boolean "foil"
-      t.string "group_name"
-      t.decimal "normal_low_price"
-      t.decimal "normal_mid_price"
-      t.decimal "normal_high_price"
-      t.decimal "normal_market_price"
-      t.decimal "foil_low_price"
-      t.decimal "foil_mid_price"
-      t.decimal "foil_high_price"
-      t.decimal "foil_market_price"
+      t.integer "magic_the_gatherig_card_id"
       t.integer "amount"
       t.integer "user_id"
       t.timestamps
