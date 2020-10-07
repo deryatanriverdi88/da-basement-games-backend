72a558112127f6f11f795afc8e7d20f1ebcf2579
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 13:27:07 -0400

    adds has_many :magic_the_gatherig_card to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :favorite_cards, dependent: :destroy
+    has_many :magic_the_gatherig_cards, through: :favorite_cards
     has_secure_password
 
     validates :email, uniqueness: true
