1ef617b7bf48c94f4c3c66fe2c1a1c950a113a56
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 13:30:46 -0400

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2020_10_06_031056) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "username"
+    t.string "role"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
