a621d2160cb972bf9bf9a75f4c804027f5f4d932
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-07 21:37:16 -0400

    updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,79 +11,79 @@
 
 # ENV["api_private_key"]
 # ENV["api_public_key"]
-# payload = "grant_type=client_credentials&client_id="+ENV["api_public_key"]+"&client_secret="+ENV["api_private_key"]
+payload = "grant_type=client_credentials&client_id="+ENV["api_public_key"]+"&client_secret="+ENV["api_private_key"]
 
-# token = RestClient.post("https://api.tcgplayer.com/token", payload  ,headers={"content-type": "application/x-www-form-urlencoded"})
+token = RestClient.post("https://api.tcgplayer.com/token", payload  ,headers={"content-type": "application/x-www-form-urlencoded"})
 
-# token = JSON.parse(token)
-# @access_token = token['access_token']
+token = JSON.parse(token)
+@access_token = token['access_token']
 
-# def all_cards
-#     length = 0
-#     response = RestClient.get 'https://api.tcgplayer.com/catalog/products?categoryId=1&productTypes=cards', {:Authorization => 'Bearer '+ @access_token}
-#     length = JSON.parse(response)['totalItems']
-#     offset = 0
-#     json = []
-#     while offset < length do
-#         response = RestClient.get('https://api.tcgplayer.com/catalog/products?categoryId=1&getExtendedFields=true&productTypes=cards&offset='+offset.to_s+'&limit=100', {:Authorization => 'Bearer '+ @access_token}){ |response, request, result, &block|
-#                         case response.code
-#                         when 200
-#                             json.push(JSON.parse(response)['results'])
-#                         when 404
-#                             puts offset, count
-#                         else
-#                           response.return!(request, result, &block)
-#                         end }
-#         offset += 100
-#     end
-#     return json.flatten
-# end
+def all_cards
+    length = 0
+    response = RestClient.get 'https://api.tcgplayer.com/catalog/products?categoryId=1&productTypes=cards', {:Authorization => 'Bearer '+ @access_token}
+    length = JSON.parse(response)['totalItems']
+    offset = 0
+    json = []
+    while offset < length do
+        response = RestClient.get('https://api.tcgplayer.com/catalog/products?categoryId=1&getExtendedFields=true&productTypes=cards&offset='+offset.to_s+'&limit=100', {:Authorization => 'Bearer '+ @access_token}){ |response, request, result, &block|
+                        case response.code
+                        when 200
+                            json.push(JSON.parse(response)['results'])
+                        when 404
+                            puts offset, count
+                        else
+                          response.return!(request, result, &block)
+                        end }
+        offset += 100
+    end
+    return json.flatten
+end
 
-# def check_rarity(values)
-#     rarity = ""
-#     values.each do |value|
-#         if value['name'] === "Rarity"
-#             if value['value'] == 'U'
-#                 rarity = 'Uncommon'
-#             elsif value['value'] == 'T'
-#                 rarity = 'Token'
-#             elsif value['value'] == 'S'
-#                 rarity = 'Special'
-#             elsif value['value'] == 'P'
-#                 rarity = 'Promo'
-#             elsif value['value'] == 'M'
-#                 rarity = 'Mythic'
-#             elsif value['value'] == 'L'
-#                 rarity = 'Land'
-#             elsif value['value'] == 'C'
-#                 rarity = 'Common'
-#             elsif value['value'] == 'R'
-#                 rarity = 'Rare'
-#             end
-#         end
-#     end
-#     return rarity
-# end
+def check_rarity(values)
+    rarity = ""
+    values.each do |value|
+        if value['name'] === "Rarity"
+            if value['value'] == 'U'
+                rarity = 'Uncommon'
+            elsif value['value'] == 'T'
+                rarity = 'Token'
+            elsif value['value'] == 'S'
+                rarity = 'Special'
+            elsif value['value'] == 'P'
+                rarity = 'Promo'
+            elsif value['value'] == 'M'
+                rarity = 'Mythic'
+            elsif value['value'] == 'L'
+                rarity = 'Land'
+            elsif value['value'] == 'C'
+                rarity = 'Common'
+            elsif value['value'] == 'R'
+                rarity = 'Rare'
+            end
+        end
+    end
+    return rarity
+end
 
-# def check_text(values)
-#     text = ''
-#     values.each do |value|
-#         if value['name'] == "OracleText"
-#             text = value['value']
-#         end
-#     end
-#     return text
-# end
+def check_text(values)
+    text = ''
+    values.each do |value|
+        if value['name'] == "OracleText"
+            text = value['value']
+        end
+    end
+    return text
+end
 
-# def check_sub_type(values)
-#     sub_type = ''
-#     values.each do |value|
-#         if value['name'] == "SubType"
-#             sub_type = value['value']
-#         end
-#     end
-#     return sub_type
-# end
+def check_sub_type(values)
+    sub_type = ''
+    values.each do |value|
+        if value['name'] == "SubType"
+            sub_type = value['value']
+        end
+    end
+    return sub_type
+end
 
 all_cards.sort_by(|card| card.id).slice(0, 20000).each do |card|
         puts 'card => ', card
