ace94842165e34c62bca812144403c884b0ed6d0
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-10-09 23:03:54 -0400

    adds validations of amount attribute for FavoriteCard model

--- app/models/favorite_card.rb
+++ app/models/favorite_card.rb
@@ -1,4 +1,7 @@
 class FavoriteCard < ApplicationRecord
     belongs_to :user
     belongs_to :magic_the_gatherig_card
+
+    validates :amount, presence: true
+    validates :amount, numericality: { other_than: 0 }
 end
